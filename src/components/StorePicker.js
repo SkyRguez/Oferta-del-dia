4a281b888ace8a4026a9ce0b91b008e9cef5b2ea
Sara Rodríguez Moldes <raheld12@gmail.com>
2018-11-07 11:37:37 +0100

    Add CSS in JS

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,7 +4,7 @@ class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
-        <h1>Please Enter a Store</h1>
+        <h2>Please Enter a Store</h2>
         <input type="text" required placeholder="Store Name" />
         <button type="submit">
           <span role="img" aria-labelledby="sushi-box">
