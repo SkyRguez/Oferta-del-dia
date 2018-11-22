a1d9772cb2015779007a06e65088bfe0f826d5ed
Sara Rodríguez Moldes <raheld12@gmail.com>
2018-11-22 14:42:40 +0100

    First attempt at total order

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
+  totalReducer = (subTotal, value) => {
+    const unitPrice = this.props.pescados[value].price;
+    const quantity = this.props.pedido[value];
+
+    return quantity * unitPrice + subTotal;
+  };
   render() {
+    const pedidoIds = Object.keys(this.props.pedido);
+    const total = pedidoIds.reduce(this.totalReducer, 0);
+
     return (
-      <div>
+      <div className="order-wrap">
         <h2>Your Order</h2>
+        {pedidoIds}
+        <div className="total">
+          Total:
+          <strong>{formatPrice(total)}</strong>
+        </div>
       </div>
     );
   }
