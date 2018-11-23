5c26d03384f3f001265889cb4c1d6ba15534a8f7
Sara Rodríguez Moldes <raheld12@gmail.com>
2018-11-23 10:31:37 +0100

    Show order

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,13 +1,37 @@
 import React from "react";
 import { formatPrice } from "../helpers";
+import { link } from "fs";
 
 class Order extends React.Component {
   totalReducer = (subTotal, value) => {
-    const unitPrice = this.props.fishes[value].price;
+    const fish = this.props.fishes[value];
     const quantity = this.props.order[value];
+    const isAvailable = fish.status === "available";
 
-    return quantity * unitPrice + subTotal;
+    if (isAvailable) {
+      return quantity * fish.price + subTotal;
+    } else {
+      return subTotal;
+    }
   };
+
+  renderOrder = orderKey => {
+    const quantity = this.props.order[orderKey];
+    const fish = this.props.fishes[orderKey];
+    const isAvailable = fish.status === "available";
+
+    if (isAvailable) {
+      return (
+        <li key={orderKey}>
+          {quantity} lbs {fish.name}
+          {formatPrice(quantity * fish.price)}
+        </li>
+      );
+    } else {
+      return <li key={orderKey}>Sorry, {fish.name} is not available</li>;
+    }
+  };
+
   render() {
     const orderIds = Object.keys(this.props.order);
     const total = orderIds.reduce(this.totalReducer, 0);
@@ -15,6 +39,7 @@ class Order extends React.Component {
     return (
       <div className="order-wrap">
         <h2>Your Order</h2>
+        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
         <div className="total">
           Total:
           <strong>{formatPrice(total)}</strong>
